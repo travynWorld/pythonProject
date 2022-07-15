5c8d7c11419e99526d97f96f06e891dd55a2d6ca
travynWorld <92635150+travynWorld@users.noreply.github.com>
2022-07-15 19:53:22 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,90 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('xuyang')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#
+# x = 10
+# y = ['1', '2', x]
+# x = 1234
+# print(y)
+#
+#
+#
+# #与用户的交互
+# username = input("请输入用户名：")
+# print(username)
+#
+
+# 格式化输出
+# res = "my name is %s, my age is %s" %('xuyang', '19')
+# print(res)
+#
+# #以字典的形式传值
+#
+# res1 = "my name is %(name)s, my age is %(age)s" %{"age":'18', "name":'xuyang'}
+# print((res1))
+
+# 成员运算符
+
+# print('a' in "aaaaaa")
+#
+# print(111 in {'k1': 111, 'k2': 222})
+#
+# print('k1' in {'k1': 111, 'k2': 222})
+
+
+# 身份运算符
+# is 判断id是否相等
+
+# 流程控制 if
+
+# a = ['aaaaaa',11]
+# b = 'bbbbbb'
+# # a.append(b)
+# # print(a)
+#
+# a.extend(b)
+# print(a)
+#
+# a.pop(0)
+
+
+# l = [0,1]
+# m = list(product(l, repeat=4))
+# print(m)
+#
+# count = len(m)
+# print("组合和次数：",count)
+# a = m[1]
+# print(a[3])
+
+
+# 排列组合
+# from itertools import product
+#
+#
+# def combination(a,b):
+#     l = [a, b]
+#     m = list(product(l, repeat=4))
+#     c = len(m)
+#     print("生成的组合为：\n", m)
+#     print("组合的数量为：", c)
+#     return (m, c)
+#
+#
+# combination(1,0)
+
+# 集合
+
